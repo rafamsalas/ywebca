eccdf2b85bf28d411b5f1d98781f417a48266e26
Rafael Martinez-Salas <martinezsalasrafael@gmail.com>
2015-07-20 09:42:47 -0500

    Adds answers for some exercises for chapter 03

--- chapter02/problem1.js
+++ chapter02/problem1.js
@@ -1,13 +1,10 @@
-function multiplesSum(){
+function problem1 ()  {
 	//Find the sum of all the multiples of 3 or 5 below 1000.
-	var total = 0;
-	for(var i = 0; i < 1000; i++){
-		if (i % 3 == 0){
-		total += i;
-		}
-		if (i % 5 == 0) {
-			total +=i;
-		}
-	}
-	console.log(total);
-};
+	var sum = 0;
+    for(var  i=0; i<1000; i++) {
+        if ( (i%3 == 0) || (i%5 == 0) ){
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
